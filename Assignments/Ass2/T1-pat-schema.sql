a3832c013aa6e8f484e2d1f75336aa2d9dc0033a
ffou0003 <ffou0003@student.monash.edu>
2024-10-27 18:39:38 +1100

    official table created, pk added

--- Assignments/Ass2/T1-pat-schema.sql
+++ Assignments/Ass2/T1-pat-schema.sql
@@ -18,6 +18,35 @@
 
 -- OFFICIAL
 
+-- DROP TABLE official CASCADE CONSTRAINTS;
+-- DROP TABLE vehicle CASCADE CONSTRAINTS;
+-- DROP TABLE trip CASCADE CONSTRAINTS;
+
+CREATE TABLE official (
+    off_id          NUMBER(4) NOT NULL,
+    off_given       VARCHAR2(30),
+    off_family      VARCHAR2(30),
+    cr_ioc_code     CHAR(3) NOT NULL,
+    off_cdm         NUMBER(4)
+);
+
+COMMENT ON COLUMN official.off_id IS
+    'Identifier for an official';
+
+COMMENT ON COLUMN official.off_given IS
+    'Given name for the official';
+
+COMMENT ON COLUMN official.off_family IS
+    'Family name for the official';
+
+COMMENT ON COLUMN official.cr_ioc_code IS
+    'IOC country code for the official';
+
+COMMENT ON COLUMN official.off_cdm IS
+    'Identifier for Chef De Mission for the official';
+
+ALTER TABLE official ADD CONSTRAINT official_pk PRIMARY KEY (off_id);
+--ck check constraints?
 
 -- VEHICLE
 
