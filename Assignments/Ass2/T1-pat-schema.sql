80edc2ce3c97159a50822e7290d30a575b4bd357
ffou0003 <ffou0003@student.monash.edu>
2024-10-27 19:21:13 +1100

    trip table (no pk yet)

--- Assignments/Ass2/T1-pat-schema.sql
+++ Assignments/Ass2/T1-pat-schema.sql
@@ -80,6 +80,54 @@ COMMENT ON COLUMN official.vm_model_id IS
 ALTER TABLE vehicle ADD CONSTRAINT vehicle_pk PRIMARY KEY (veh_vin);
 
 -- TRIP
+CREATE TABLE trip (
+    trip_id                 NUMBER(4) NOT NULL,
+    trip_nopassengers       NUMBER(2) NOT NULL,
+    trip_int_pickupdt       DATE NOT NULL,
+    trip_act_pickupdt       DATE,
+    trip_int_dropoffdt      DATE NOT NULL,
+    trip_act_dropoffdt      DATE,
+    veh_vin                 CHAR(17) NOT NULL,
+    driver_id               NUMBER(4) NOT NULL,
+    pickup_locn_id          NUMBER(3) NOT NULL,
+    dropoff_locn_id         NUMBER(4) NOT NULL,
+    lang_iso_code           CHAR(2) NOT NULL,
+    off_id                  NUMBER(4) NOT NULL
+);
 
+COMMENT ON COLUMN trip.trip_id IS
+    'Identifier for a trip';
 
+COMMENT ON COLUMN trip.trip_nopassengers IS
+    'Number of passengers for the trip';
+
+COMMENT ON COLUMN trip.trip_int_pickupdt IS
+    'Intended pickup date and time for the trip';
+
+COMMENT ON COLUMN trip.trip_act_pickupdt IS
+    'Actual pickup date and time for the trip';
+
+COMMENT ON COLUMN trip.trip_int_dropoffdt IS
+    'Intended drop-off date and time for the trip';
+
+COMMENT ON COLUMN trip.trip_act_dropoffdt IS
+    'Actual drop-off date and time for the trip';
+
+COMMENT ON COLUMN trip.veh_vin IS
+    'Identifier for a vehicle';
+
+COMMENT ON COLUMN trip.driver_id IS
+    'Identifier for a driver';
+
+COMMENT ON COLUMN trip.pickup_locn_id IS
+    'Identifier for the pick-up location';
+
+COMMENT ON COLUMN trip.dropoff_locn_id IS
+    'Identifier for the drop-off location';
+
+COMMENT ON COLUMN trip.lang_iso_code IS
+    'ISO639-1 two-character language codes';
+
+COMMENT ON COLUMN trip.off_id IS
+    'Identifier for an official';
 -- Add all missing FK Constraints below here
