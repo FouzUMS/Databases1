15538d564543957d031bcdc0d6d94f995ff4793c
ffou0003 <ffou0003@student.monash.edu>
2024-11-01 06:22:07 +1100

    INSERT data 20 rows for trip table

--- Assignments/Ass2/T2-pat-insert.sql
+++ Assignments/Ass2/T2-pat-insert.sql
@@ -327,6 +327,35 @@ INSERT INTO vehicle (
 --has 4 dates. Will need:
 --to_date('______ __:__:__', 'dd-Mon-yyyy hh24:mi:ss')
 
+-- INSERT INTO trip (
+--     trip_id,
+--     trip_nopassengers,
+--     trip_int_pickupdt,
+--     trip_act_pickupdt,
+--     trip_int_dropoffdt,
+--     trip_act_dropoffdt,
+--     veh_vin,
+--     driver_id,
+--     pickup_locn_id,
+--     dropoff_locn_id,
+--     lang_iso_code,
+--     off_id
+-- ) VALUES (
+--     1,
+--     6,
+--     to_date('22-Jul-2024 09:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+--     to_date('22-Jul-2024 09:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+--     to_date('22-Jul-2024 13:15:00', 'dd-Mon-yyyy hh24:mi:ss'),
+--     to_date('22-Jul-2024 13:15:00', 'dd-Mon-yyyy hh24:mi:ss'),
+--     82738263826325651,
+--     2002,
+--     105,
+--     110,
+--     'ml',
+--     5
+-- );
+
+--1
 INSERT INTO trip (
     trip_id,
     trip_nopassengers,
@@ -353,6 +382,558 @@ INSERT INTO trip (
     110,
     'ml',
     5
+);
+
+--2
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    2,
+    13,
+    to_date('22-Jul-2024 09:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('22-Jul-2024 09:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('22-Jul-2024 13:15:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('22-Jul-2024 13:15:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    28786826725265628,
+    2007,
+    105,
+    110,
+    'fr',
+    5
+);
+
+--3
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    3,
+    4,
+    to_date('20-Jul-2024 07:20:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('20-Jul-2024 07:35:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('20-Jul-2024 08:40:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('20-Jul-2024 08:45:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    82738263826325651,
+    2002,
+    103,
+    120,
+    'zh',
+    2
+);
+
+--4
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    4,
+    13,
+    to_date('11-Aug-2024 15:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('11-Aug-2024 15:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('12-Aug-2024 01:13:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('12-Aug-2024 01:13:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    28786826725265628,
+    2013,
+    105,
+    107,
+    'fr',
+    1
+);
+
+--5
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    5,
+    6,
+    to_date('11-Aug-2024 15:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('11-Aug-2024 15:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('12-Aug-2024 01:13:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('12-Aug-2024 01:13:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    82738263826325651,
+    2004,
+    105,
+    107,
+    'sd',
+    1
+);
+
+--6
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    6,
+    7,
+    to_date('23-Jul-2024 17:05:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('23-Jul-2024 17:05:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('23-Jul-2024 17:50:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('23-Jul-2024 17:50:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    27492267998911716,
+    2002,
+    101,
+    110,
+    'fr',
+    6
+);
+
+--7
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    7,
+    9,
+    to_date('22-Jul-2024 19:40:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('22-Jul-2024 19:40:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('22-Jul-2024 23:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('22-Jul-2024 23:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    86125279171080189,
+    2004,
+    105,
+    110,
+    'fr',
+    2
+);
+
+--8
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    8,
+    7,
+    to_date('26-Jul-2024 10:10:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('26-Jul-2024 10:10:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('26-Jul-2024 10:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('26-Jul-2024 10:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    27492267998911716,
+    2002,
+    104,
+    110,
+    'sd',
+    1
+);
+
+--9
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    9,
+    11,
+    to_date('24-Jul-2024 10:10:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('24-Jul-2024 10:10:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('25-Jul-2024 18:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('25-Jul-2024 18:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    86125279171080189,
+    2013,
+    105,
+    110,
+    'zh',
+    1
+);
+
+--10
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    10,
+    7,
+    to_date('21-Jul-2024 18:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('21-Jul-2024 18:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('21-Jul-2024 19:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('21-Jul-2024 19:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    19781671156197253,
+    2013,
+    103,
+    107,
+    'zh',
+    2
+);
+
+--11
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    11,
+    6,
+    to_date('21-Jul-2024 12:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('21-Jul-2024 12:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('21-Jul-2024 13:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('21-Jul-2024 13:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    28947294463762929,
+    2007,
+    101,
+    110,
+    'ne',
+    3
+);
+
+
+--12
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    12,
+    7,
+    to_date('23-Jul-2024 17:05:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('23-Jul-2024 17:05:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('23-Jul-2024 17:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('23-Jul-2024 17:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    28947294463762929,
+    2002,
+    101,
+    107,
+    'fr',
+    4
+);
+
+--13
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    13,
+    10,
+    to_date('27-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('27-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('27-Jul-2024 09:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('27-Jul-2024 09:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    86125279171080189,
+    2004,
+    110,
+    101,
+    'fr',
+    4
+);
+
+--14
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    14,
+    13,
+    to_date('27-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('27-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('27-Jul-2024 09:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('27-Jul-2024 09:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    99173626516397281,
+    2015,
+    110,
+    101,
+    'fr',
+    4
+);
+
+--15
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    15,
+    6,
+    to_date('25-Jul-2024 11:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('25-Jul-2024 11:15:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('25-Jul-2024 11:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('25-Jul-2024 11:40:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    82738263826325651,
+    2007,
+    105,
+    110,
+    'id',
+    5
+);
+
+--16
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    16,
+    6,
+    to_date('28-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('28-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('28-Jul-2024 07:45:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('28-Jul-2024 07:45:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    19781671156197253,
+    2013,
+    105,
+    110,
+    'fa',
+    8
+);
+
+--17
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    17,
+    5,
+    to_date('28-Jul-2024 17:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('28-Jul-2024 17:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('28-Jul-2024 19:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('28-Jul-2024 19:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    82738263826325651,
+    2015,
+    101,
+    120,
+    'ne',
+    4
+);
+
+--18
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    18,
+    35,
+    to_date('26-Jul-2024 20:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('26-Jul-2024 20:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('26-Jul-2024 20:45:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('26-Jul-2024 20:45:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    36482739282080328,
+    2003,
+    104,
+    110,
+    'zh',
+    1
+);
+
+--19
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    19,
+    13,
+    to_date('29-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('29-Jul-2024 07:00:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('29-Jul-2024 08:15:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('29-Jul-2024 08:15:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    28786826725265628,
+    2004,
+    103,
+    120,
+    'fa',
+    8
+);
+
+--20
+INSERT INTO trip (
+    trip_id,
+    trip_nopassengers,
+    trip_int_pickupdt,
+    trip_act_pickupdt,
+    trip_int_dropoffdt,
+    trip_act_dropoffdt,
+    veh_vin,
+    driver_id,
+    pickup_locn_id,
+    dropoff_locn_id,
+    lang_iso_code,
+    off_id
+) VALUES (
+    20,
+    10,
+    to_date('29-Jul-2024 18:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('29-Jul-2024 18:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('29-Jul-2024 19:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    to_date('29-Jul-2024 19:30:00', 'dd-Mon-yyyy hh24:mi:ss'),
+    86125279171080189,
+    2002,
+    103,
+    110,
+    'ml',
+    6
 );
 
 
