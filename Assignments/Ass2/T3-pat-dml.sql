b171d29f5f1656433527a953a8cab303f28ae32d
ffou0003 <ffou0003@student.monash.edu>
2024-11-02 00:13:01 +1100

    T3: Part b complete; no errors

--- Assignments/Ass2/T3-pat-dml.sql
+++ Assignments/Ass2/T3-pat-dml.sql
@@ -19,13 +19,40 @@ CREATE SEQUENCE off_trip_seq
     START WITH 100
         INCREMENT BY 10;
 
-INSERT INTO official VALUES (off_trip_seq.nextval);
-
-INSERT INTO trip VALUES (off_trip_seq.nextval);
+-- INSERT INTO official VALUES (off_trip_seq.nextval);
+-- INSERT INTO trip VALUES (off_trip_seq.nextval);
 
 /*(b)*/
 
+INSERT INTO OFFICIAL (
+    OFF_ID,
+    OFF_GIVEN,
+    OFF_FAMILY,
+    CR_IOC_CODE,
+    OFF_CDM
+) VALUES (
+    OFF_TRIP_SEQ.NEXTVAL,
+    'Franklin',
+    'Gateau',
+    (SELECT CR_IOC_CODE FROM COUNTRY_REGION WHERE upper(CR_NAME) = upper('St Vincent and the Grenadines')),
+    NULL
+);
 
+INSERT INTO vehicle (
+    veh_vin,
+    veh_rego,
+    veh_year,
+    veh_curr_odo,
+    veh_nopassengers,
+    vm_model_id
+) VALUES (
+    '1C4SDHCT9FC614231',
+    'JPNQ814',
+    to_date(2016,'yyyy'),
+    62319,
+    6,
+    (SELECT vm_model_id FROM VEHICLE_MODEL WHERE upper(vm_model) = 'ALPHARD')
+);
 
 /*(c)*/
 
