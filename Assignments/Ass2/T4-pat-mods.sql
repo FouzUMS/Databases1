898dcc0ee1abfec517e2a4e93cd8a9d0a5af8099
ffou0003 <ffou0003@student.monash.edu>
2024-11-02 16:43:35 +1100

    T4: a is functional without errors

--- Assignments/Ass2/T4-pat-mods.sql
+++ Assignments/Ass2/T4-pat-mods.sql
@@ -1,8 +1,8 @@
 /*****PLEASE ENTER YOUR DETAILS BELOW*****/
 --T4-pat-mods.sql
 
---Student ID:
---Student Name:
+--Student ID: Fouz Ul Mubeen
+--Student Name: 35278307
 
 
 /* Comments for your marker:
@@ -13,10 +13,42 @@
 */
 
 /*(a)*/
+--Previous state
+ALTER TABLE official
+DROP CONSTRAINT chk_off_role;
 
+ALTER TABLE official
+DROP COLUMN off_role;
 
+SELECT * FROM official;
+DESC official;
 
+--Column off_role added
+ALTER TABLE official ADD (
+    off_role VARCHAR2(15) DEFAULT 'General',
+    constraint chk_off_role check (off_role in (
+        'General',
+        'Administrator',
+        'Head Coach',
+        'Coach',
+        'Physician'
+    )
+    )
+);
+
+COMMENT ON COLUMN official.off_role IS
+    'Role of the official';
+
+--off_role for all CDMs changes to 'Administrator'
+SELECT * FROM official;
+DESC official;
+
+UPDATE official
+SET 
+    off_role = 'Administrator'
+WHERE 
+    off_cdm IS null;
+
+SELECT * FROM official;
 
 /*(b)*/
-
-
